513d67e98bce929636cfc53e931adaac380ad886
Prajon-Adhikari <prajonadhikari44@gmail.com>
2025-12-12 20:47:48 +0545

    changing

--- frontend/src/components/SignUpForm.jsx
+++ frontend/src/components/SignUpForm.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
+  import.meta.env.VITE_API_BASE_URL || "https://blood-ofxk.onrender.com/api";
 
 export default function SignUpForm() {
   const [form, setForm] = useState({ fullName: "", email: "", password: "" });
